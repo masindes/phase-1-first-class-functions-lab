4c1633a5c04dea4dc952e5942753949698362ada
masindes <maasinde.sylvester@gmail.com>
2024-09-26 18:21:20 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    const newArr = [];
+    for (let i = 0; i < 2; i++) {
+      newArr.push(drivers[i]);
+    }
+    return newArr;
+  };
+  const returnLastTwoDrivers = (drivers) => {
+    const newArr = [];
+    for (let i = drivers.length - 2; i < drivers.length; i++) {
+      newArr.push(drivers[i]);
+    }
+    return newArr;
+  };
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  const createFareMultiplier = (multiplier) => {
+    return (fare) => fare * multiplier;
+  };
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  const selectDifferentDrivers = (arrayOfDrivers, functionToInvoke) => {
+    return functionToInvoke(arrayOfDrivers);
+  };
